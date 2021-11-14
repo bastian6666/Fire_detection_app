30191fcced20a9b638ee262b8b93b3ac54311c35
bastian6666 <57109930+bastian6666@users.noreply.github.com>
2021-11-14 12:51:47 -0600

    Initial Commit

---
+++ Fire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fire
+//
+//  Created by Sebastian Sanchez Bernal on 14/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
